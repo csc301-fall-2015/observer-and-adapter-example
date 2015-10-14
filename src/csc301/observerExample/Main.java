8a72781101f2346094b9ff6a7278c89cba671619
Joey Freund <joeyfreund@gmail.com>
2015-10-13 22:46:40 -0400

    Breaking Application into two classes, Application and Main.

---
+++ src/csc301/observerExample/Main.java
@@ -0,0 +1,17 @@
+package csc301.observerExample;
+
+import java.math.BigDecimal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application application = new Application();
+		Stock s = new Stock("AMZN", new BigDecimal("296.52"));
+		
+		s.addObserver(application);
+				
+		s.setPrice(s.getPrice().multiply(new BigDecimal("1.02")));
+		s.setPrice(s.getPrice().multiply(new BigDecimal("0.97")));
+	}
+
+}
